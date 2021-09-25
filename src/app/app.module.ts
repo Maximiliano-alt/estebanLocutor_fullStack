0624b55e9cb99cf54bc18b93dd48738eb0e06202
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2021-09-24 22:52:50 -0300

    basic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { BannerComponent } from './home/components/banner/banner.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ServicesLocutorComponent } from './services-locutor/services-locutor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ContactComponent,
+    GalleryComponent,
+    ServicesLocutorComponent
   ],
   imports: [
     BrowserModule,
