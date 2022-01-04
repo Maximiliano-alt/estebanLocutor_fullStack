f10305782fb41e2c81f80c51c7c6d46b7d106902
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-01-04 02:23:26 -0300

    swiper added

--- src/app/home/components/banner/banner.component.ts
+++ src/app/home/components/banner/banner.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
+  @ViewChild('carousel', { static: true })
 
   images: string[] = [
     'assets/images/esteban-banner.png',
-
+    'assets/images/Screenshot-esteban.png',
     ];
+    paused = false;
+    unpauseOnArrow = false;
+    pauseOnIndicator = false;
+    pauseOnHover = true;
+    pauseOnFocus = true;
+    onSwiper(swiper:any) {
+      console.log(swiper);
+    }
+    onSlideChange() {
+      console.log('slide change');
+    }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
 
 }
