5a2d3b181ab5fe6afb12b6c4e180b2a8e4ef81dc
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-01-04 02:24:02 -0300

    forms module added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatIconModule} from '@angular/material/icon';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule
   ],
-  exports:[MatIconModule]
+  exports:[
+    MatIconModule,
+    FormsModule
+  ]
 })
 export class MaterialModule { }
