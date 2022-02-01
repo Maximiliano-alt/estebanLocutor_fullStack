5c8bd7f8fc2442b108a9b1753a4e4fa769e9e42b
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-02-01 11:30:22 -0300

    new reactive form

--- src/app/contract/components/contract.component.ts
+++ src/app/contract/components/contract.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup,FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 @Component({
   selector: 'app-contract',
   templateUrl: './contract.component.html',
-  styleUrls: ['./contract.component.css']
+  styleUrls: ['./contract.component.css'],
+  encapsulation : ViewEncapsulation.None,
 })
 export class ContractComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createFormGroup();
+  }
 
   ngOnInit(): void {
   }
+  saveInfo(event: Event){
+    event.preventDefault();
+    if(this.form.valid){
+      console.log(this.form.value);
+    }
+    else{
+      console.log('envio no valido')
+    }
 
+  }
+  private createFormGroup(){
+    this.form = this.formBuilder.group({
+      name:['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      email:['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      country:['', [Validators.required]],
+      city:['', [Validators.required]],
+      enterprise:['', [Validators.required]],
+      service:['', [Validators.required]],
+      message:['', [Validators.required]]
+    })
+  }
+  getEmail() {
+    return this.form.get('email');
+}
+  sendToEmail(){
+    if(this.form.valid){
+
+    }
+  }
 }
