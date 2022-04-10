8ace7f72cc23ccea96a8cd3fce5eb4ce08433d57
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-04-10 00:58:05 -0400

    style and text changes

--- src/app/services-locutor/components/services-locutor.component.ts
+++ src/app/services-locutor/components/services-locutor.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-services-locutor',
   templateUrl: './services-locutor.component.html',
-  styleUrls: ['./services-locutor.component.css']
+  styleUrls: ['./services-locutor.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ServicesLocutorComponent implements OnInit {
 
