164c942604c87983c2be8e380e13b87f9c6a5546
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-05-01 12:23:03 -0400

    refactoring year and text

--- src/app/contact/components/contact.component.ts
+++ src/app/contact/components/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import SwiperCore, { Pagination } from "swiper";
 import { SwiperComponent } from "swiper/angular";
-
+import { DatePipe } from '@angular/common';
 SwiperCore.use([Pagination]);
 @Component({
   selector: 'app-contact',
@@ -10,7 +10,10 @@ SwiperCore.use([Pagination]);
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ContactComponent implements OnInit {
-
+  date: Date = new Date();
+  getDate() {
+    return this.date.getFullYear();
+  }
   constructor() { }
 
   ngOnInit(): void {
